0f9d8a5f3919a2cccac713e634fa1256bb45532d
Suraj Kumar Singh <42529024+s0oraj@users.noreply.github.com>
2024-12-23 14:12:07 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+impoty HomePage from "./pages/HomePage";
 import PatternDetail from "./pages/PatternDetail";
 
 const queryClient = new QueryClient();
@@ -15,7 +16,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/pattern/:id" element={<PatternDetail />} />
         </Routes>
       </BrowserRouter>
