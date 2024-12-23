3ca189f431964b3764acd787f4420fdd5a4a1f1c
s0oraj12 <myselfsuraj123@gmail.com>
2024-12-24 01:12:03 +0530

    Update Subpattern.tsx

--- src/components/patterns/pattern-detail-components/questions-list/Subpattern.tsx
+++ src/components/patterns/pattern-detail-components/questions-list/Subpattern.tsx
@@ -3,7 +3,7 @@ import { useState, useRef, useEffect } from "react";
 import { ChevronDown, ChevronUp } from 'lucide-react';
 import { Subpattern } from "@/types";
 import { QuestionComponent } from "./Question";
-import { AnimationDialog } from "./AnimationDialog";
+import AnimationDialog from "./AnimationDialog";
 import { countingAnimations } from "@/data/patterns/counting/animations";
 
 interface SubpatternProps {
