15e80d9c44e037d887b685239c3dea52fe0639cc
Suraj Kumar Singh <42529024+s0oraj@users.noreply.github.com>
2024-12-23 16:45:01 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-//import Index from "./pages/Index";
-import HomePage from "./pages/HomePage";
+import Index from "./pages/Index";
+//import HomePage from "./pages/HomePage";
 import PatternDetail from "./pages/PatternDetail";
 
 const queryClient = new QueryClient();
@@ -16,7 +16,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<Index />} />
           <Route path="/pattern/:id" element={<PatternDetail />} />
         </Routes>
       </BrowserRouter>
