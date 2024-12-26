b29b98a28fc4e59c27e963553b23bc7cf23941f8
Suraj Kumar Singh <42529024+s0oraj@users.noreply.github.com>
2024-12-26 18:54:48 +0530

    Update index.ts

--- src/data/patterns/binary-indexed-tree-pattern/animations/index.ts
+++ src/data/patterns/binary-indexed-tree-pattern/animations/index.ts
@@ -1,23 +1,27 @@
+import { basicRangeOperationsAnimation } from './basic-range-operations/data';
+import BasicRangeOperationsVisualizer from './basic-range-operations/visualizer';
+import { orderStatisticsAnimation } from './order-statistics/data';
+import OrderStatisticsVisualizer from './order-statistics/visualizer';
+import { twoDOperationsAnimation } from './bit-2d-operations/data';
+import TwoDOperationsVisualizer from './bit-2d-operations/visualizer';
+import { multipleBITsAnimation } from './multiple-bits/data';
+import MultipleBITsVisualizer from './multiple-bits/visualizer';
+import { advancedApplicationsAnimation } from './bit-advanced-applications/data';
+import AdvancedApplicationsVisualizer from './bit-advanced-applications/visualizer';
 
 export const visualizers = {
+  'basic-range-operations': BasicRangeOperationsVisualizer,
+  'order-statistics': OrderStatisticsVisualizer,
+  'bit-2d-operations': TwoDOperationsVisualizer,
+  'multiple-bits': MultipleBITsVisualizer,
+  'bit-advanced-applications': AdvancedApplicationsVisualizer,
 };
 
-export const countingAnimations = {
-  "basic-counter-operations": placeholderAnimation,
-  "frequency-distribution": placeholderAnimation,
-  "window-based-counting": placeholderAnimation,
-  'state-based-counting': placeholderAnimation,
-  "population-tracking": placeholderAnimation,
-  "advanced-counting": placeholderAnimation
+export const binaryIndexedTreeAnimations = {
+  "basic-range-operations": basicRangeOperationsAnimation,
+  "order-statistics": orderStatisticsAnimation,
+  "bit-2d-operations": twoDOperationsAnimation,
+  "multiple-bits": multipleBITsAnimation,
+  "bit-advanced-applications": advancedApplicationsAnimation,
 };
-const placeholderAnimation = {
-  title: "Coming Soon",
-  steps: [{
-    description: "Animation under development",
-    code: "// TODO",
-    visualization: {
-      elements: [],
-      highlightIndices: []
-    }
-  }]
-};
+
