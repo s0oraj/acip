64616f8f697b53be2d72095006cea189425b0696
s0oraj11 <ridesuraj@gmail.com>
2024-12-26 09:43:41 +0530

    Update index.ts

--- src/data/patterns/mo-s-algorithm-pattern/animations/index.ts
+++ src/data/patterns/mo-s-algorithm-pattern/animations/index.ts
@@ -1,23 +1,31 @@
+import { basicRangeQueryOperationsAnimation } from './basic-range-query-operations/data';
+import BasicRangeQueryOperationsVisualizer from './basic-range-query-operations/visualizer';
+
+import { frequencyBasedQueriesAnimation } from './frequency-based-queries/data';
+import FrequencyBasedQueriesVisualizer from './frequency-based-queries/visualizer';
+
+import { advancedRangeOperationsAnimation } from './advanced-range-operations/data';
+import AdvancedRangeOperationsVisualizer from './advanced-range-operations/visualizer';
+
+import { specialRangeOperationsAnimation } from './special-range-operations/data';
+import SpecialRangeOperationsVisualizer from './special-range-operations/visualizer';
+
+import { updateOperationsAnimation } from './update-operations/data';
+import UpdateOperationsVisualizer from './update-operations/visualizer';
 
 export const visualizers = {
+  'basic-range-query-operations': BasicRangeQueryOperationsVisualizer,
+  'frequency-based-queries': FrequencyBasedQueriesVisualizer,
+  'advanced-range-operations': AdvancedRangeOperationsVisualizer,
+  'special-range-operations': SpecialRangeOperationsVisualizer,
+  'update-operations': UpdateOperationsVisualizer
 };
 
-export const countingAnimations = {
-  "basic-counter-operations": placeholderAnimation,
-  "frequency-distribution": placeholderAnimation,
-  "window-based-counting": placeholderAnimation,
-  'state-based-counting': placeholderAnimation,
-  "population-tracking": placeholderAnimation,
-  "advanced-counting": placeholderAnimation
+export const mosAlgorithmAnimations = {
+  "basic-range-query-operations": basicRangeQueryOperationsAnimation,
+  "frequency-based-queries": frequencyBasedQueriesAnimation,
+  "advanced-range-operations": advancedRangeOperationsAnimation,
+  "special-range-operations": specialRangeOperationsAnimation,
+  "update-operations": updateOperationsAnimation
 };
-const placeholderAnimation = {
-  title: "Coming Soon",
-  steps: [{
-    description: "Animation under development",
-    code: "// TODO",
-    visualization: {
-      elements: [],
-      highlightIndices: []
-    }
-  }]
-};
+
