429f2466ee18c94a9a520b7964a1af90313d6f34
s0oraj11 <ridesuraj@gmail.com>
2024-12-26 12:57:50 +0530

    Update index.ts

--- src/data/patterns/clone-pattern/animations/index.ts
+++ src/data/patterns/clone-pattern/animations/index.ts
@@ -1,23 +1,27 @@
+import { basicNodeCloningAnimation } from './basic-node-cloning/data';
+import BasicNodeCloningVisualizer from './basic-node-cloning/visualizer';
+import { treeStructureCloningAnimation } from './tree-structure-cloning/data';
+import TreeStructureCloningVisualizer from './tree-structure-cloning/visualizer';
+import { graphStructureCloningAnimation } from './graph-structure-cloning/data';
+import GraphStructureCloningVisualizer from './graph-structure-cloning/visualizer';
+import { containerStructureCloningAnimation } from './container-structure-cloning/data';
+import ContainerStructureCloningVisualizer from './container-structure-cloning/visualizer';
+import { cacheStructureCloningAnimation } from './cache-structure-cloning/data';
+import CacheStructureCloningVisualizer from './cache-structure-cloning/visualizer';
 
 export const visualizers = {
+  'basic-node-cloning': BasicNodeCloningVisualizer,
+  'tree-structure-cloning': TreeStructureCloningVisualizer,
+  'graph-structure-cloning': GraphStructureCloningVisualizer,
+  'container-structure-cloning': ContainerStructureCloningVisualizer,
+  'cache-structure-cloning': CacheStructureCloningVisualizer,
 };
 
-export const countingAnimations = {
-  "basic-counter-operations": placeholderAnimation,
-  "frequency-distribution": placeholderAnimation,
-  "window-based-counting": placeholderAnimation,
-  'state-based-counting': placeholderAnimation,
-  "population-tracking": placeholderAnimation,
-  "advanced-counting": placeholderAnimation
+export const clonePatternAnimations = {
+  "basic-node-cloning": basicNodeCloningAnimation,
+  "tree-structure-cloning": treeStructureCloningAnimation,
+  "graph-structure-cloning": graphStructureCloningAnimation,
+  "container-structure-cloning": containerStructureCloningAnimation,
+  "cache-structure-cloning": cacheStructureCloningAnimation,
 };
-const placeholderAnimation = {
-  title: "Coming Soon",
-  steps: [{
-    description: "Animation under development",
-    code: "// TODO",
-    visualization: {
-      elements: [],
-      highlightIndices: []
-    }
-  }]
-};
+
