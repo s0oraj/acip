5b40b64a6e77e713261a5c6dd3e0e8f8c48e4cce
s0oraj11 <ridesuraj@gmail.com>
2024-12-26 11:13:52 +0530

    Update index.ts

--- src/data/patterns/linear-sorting-pattern/animations/index.ts
+++ src/data/patterns/linear-sorting-pattern/animations/index.ts
@@ -1,23 +1,31 @@
+import { countingSortAnimation } from './counting-sort-with-modified-rules/data';
+import CountingSortVisualizer from './counting-sort-with-modified-rules/visualizer';
+import { advancedCountingSortAnimation } from './advanced-counting-sort-applications/data';
+import AdvancedCountingSortVisualizer from './advanced-counting-sort-applications/visualizer';
+import { bucketSortAnimation } from './bucket-sort-foundations/data';
+import BucketSortVisualizer from './bucket-sort-foundations/visualizer';
+import { advancedBucketAnimation } from './advanced-bucket-applications/data';
+import AdvancedBucketVisualizer from './advanced-bucket-applications/visualizer';
+import { radixSortAnimation } from './radix-sort-implementations/data';
+import RadixSortVisualizer from './radix-sort-implementations/visualizer';
+import { advancedRadixAnimation } from './advanced-radix-applications/data';
+import AdvancedRadixVisualizer from './advanced-radix-applications/visualizer';
 
 export const visualizers = {
+  'counting-sort-with-modified-rules': CountingSortVisualizer,
+  'advanced-counting-sort-applications': AdvancedCountingSortVisualizer,
+  'bucket-sort-foundations': BucketSortVisualizer,
+  'advanced-bucket-applications': AdvancedBucketVisualizer,
+  'radix-sort-implementations': RadixSortVisualizer,
+  'advanced-radix-applications': AdvancedRadixVisualizer,
 };
 
-export const countingAnimations = {
-  "basic-counter-operations": placeholderAnimation,
-  "frequency-distribution": placeholderAnimation,
-  "window-based-counting": placeholderAnimation,
-  'state-based-counting': placeholderAnimation,
-  "population-tracking": placeholderAnimation,
-  "advanced-counting": placeholderAnimation
+export const linearSortingAnimations = {
+  "counting-sort-with-modified-rules": countingSortAnimation,
+  "advanced-counting-sort-applications": advancedCountingSortAnimation,
+  "bucket-sort-foundations": bucketSortAnimation,
+  "advanced-bucket-applications": advancedBucketAnimation,
+  "radix-sort-implementations": radixSortAnimation,
+  "advanced-radix-applications": advancedRadixAnimation,
 };
-const placeholderAnimation = {
-  title: "Coming Soon",
-  steps: [{
-    description: "Animation under development",
-    code: "// TODO",
-    visualization: {
-      elements: [],
-      highlightIndices: []
-    }
-  }]
-};
+
