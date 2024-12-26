16865cfb2a25fa401de419fb187f8015924f5433
s0oraj11 <ridesuraj@gmail.com>
2024-12-26 10:37:41 +0530

    Update index.ts

--- src/data/patterns/meet-in-the-middle-pattern/animations/index.ts
+++ src/data/patterns/meet-in-the-middle-pattern/animations/index.ts
@@ -1,23 +1,31 @@
+import { basicSubsetDivisionAnimation } from './basic-subset-division/data';
+import BasicSubsetDivisionVisualizer from './basic-subset-division/visualizer';
+
+import { sumBasedDivisionAnimation } from './sum-based-division/data';
+import SumBasedDivisionVisualizer from './sum-based-division/visualizer';
+
+import { productDivisionAnimation } from './product-division/data';
+import ProductDivisionVisualizer from './product-division/visualizer';
+
+import { advancedReconstructionAnimation } from './advanced-reconstruction/data';
+import AdvancedReconstructionVisualizer from './advanced-reconstruction/visualizer';
+
+import { stateReconstructionAnimation } from './state-reconstruction/data';
+import StateReconstructionVisualizer from './state-reconstruction/visualizer';
 
 export const visualizers = {
+  'basic-subset-division': BasicSubsetDivisionVisualizer,
+  'sum-based-division': SumBasedDivisionVisualizer,
+  'product-division': ProductDivisionVisualizer,
+  'advanced-reconstruction': AdvancedReconstructionVisualizer,
+  'state-reconstruction': StateReconstructionVisualizer
 };
 
-export const countingAnimations = {
-  "basic-counter-operations": placeholderAnimation,
-  "frequency-distribution": placeholderAnimation,
-  "window-based-counting": placeholderAnimation,
-  'state-based-counting': placeholderAnimation,
-  "population-tracking": placeholderAnimation,
-  "advanced-counting": placeholderAnimation
+export const meetInTheMiddleAnimations = {
+  "basic-subset-division": basicSubsetDivisionAnimation,
+  "sum-based-division": sumBasedDivisionAnimation,
+  "product-division": productDivisionAnimation,
+  "advanced-reconstruction": advancedReconstructionAnimation,
+  "state-reconstruction": stateReconstructionAnimation
 };
-const placeholderAnimation = {
-  title: "Coming Soon",
-  steps: [{
-    description: "Animation under development",
-    code: "// TODO",
-    visualization: {
-      elements: [],
-      highlightIndices: []
-    }
-  }]
-};
+
