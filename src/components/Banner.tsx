61d14da7343f214cf129308eedbcfbc2974535bb
Suraj Kumar Singh <42529024+s0oraj@users.noreply.github.com>
2025-01-05 23:24:14 +0530

    Update Banner.tsx

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -62,6 +62,7 @@ export const Banner = () => {
         </motion.p>
 
         <motion.div
+          className="mt-32"
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8, delay: 0.4, ease: "easeOut" }}
